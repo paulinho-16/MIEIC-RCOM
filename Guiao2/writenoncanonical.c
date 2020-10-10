3e3c7a4cf1ac4374de3ae5fccc8c74cba115be42
Paulinho <up201806505@fe.up.pt>
2020-10-10 16:02:01 +0100

    Added const header

---
+++ Guiao2/writenoncanonical.c
@@ -0,0 +1,129 @@
+/*Non-Canonical Input Processing*/
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+
+#include "const_defines.h"
+
+#define BAUDRATE B38400
+#define MODEMDEVICE "/dev/ttyS1"
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+#define FALSE 0
+#define TRUE 1
+
+#define BCC_SET A_Sender_Receiver^C_SET
+
+volatile int STOP=FALSE;
+
+int fd;
+
+int received_UA = FALSE;
+int resent_times = 0;
+
+unsigned char SET[5] = {FLAG, A_Sender_Receiver, C_SET, BCC_SET, FLAG};
+
+void send_SET() {
+  int i = 0;
+  while (i < 5) {
+    write(fd, &SET[i], 1);
+    i++;
+  }
+
+  printf("Sent: SET = 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x \n", SET[0], SET[1], SET[2], SET[3], SET[4]);
+}
+
+// ----- Signal Handlers -----
+void alarm_handler() {
+  if (!received_UA) {
+    send_SET();
+    resent_times++;
+    if (resent_times < 3)
+      alarm(3);
+    else
+      exit(1);
+  }
+}
+
+int main(int argc, char** argv)
+{
+  struct termios oldtio,newtio;
+  
+  if ( (argc < 2) || 
+        ((strcmp("/dev/ttyS0", argv[1])!=0) && 
+        (strcmp("/dev/ttyS1", argv[1])!=0) )) {
+    printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+    exit(1);
+  }
+
+  /*
+    Open serial port device for reading and writing and not as controlling tty
+    because we don't want to get killed if linenoise sends CTRL-C.
+  */
+
+  fd = open(argv[1], O_RDWR | O_NOCTTY );
+  if (fd <0) {perror(argv[1]); exit(-1); }
+
+  if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+    perror("tcgetattr");
+    exit(-1);
+  }
+
+  bzero(&newtio, sizeof(newtio));
+  newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+  newtio.c_iflag = IGNPAR;
+  newtio.c_oflag = 0;
+
+  /* set input mode (non-canonical, no echo,...) */
+  newtio.c_lflag = 0;
+
+  newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+  newtio.c_cc[VMIN]     = 1;   /* blocking read until 1 char received */
+
+  /* 
+    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
+    leitura do(s) pr�ximo(s) caracter(es)
+  */
+
+  tcflush(fd, TCIOFLUSH);
+
+  if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
+    perror("tcsetattr");
+    exit(-1);
+  }
+
+  printf("New termios structure set\n");
+
+  unsigned char UA[5];
+
+  signal(SIGALRM, alarm_handler);
+
+  send_SET();
+
+  alarm(3);
+
+  int i = 0;
+  while (i < 5) {
+    read(fd, &UA[i], 1);
+    i++;
+  }
+
+  received_UA = TRUE;
+  printf("Received: UA = 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x \n", UA[0], UA[1], UA[2], UA[3], UA[4]);
+
+  sleep(1);
+  
+  if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
+    perror("tcsetattr");
+    exit(-1);
+  }
+
+  close(fd);
+  return 0;
+}
